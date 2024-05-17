362379c00ac7da28390c02193eb0e6d2dd42a1ef
mahadev <mahadev@test.com>
2024-05-17 03:24:13 -0500

    JDK to 11

--- src/main/java/com/maha/restapi/MahaRestApiApplication.java
+++ src/main/java/com/maha/restapi/MahaRestApiApplication.java
@@ -15,9 +15,9 @@ public class MahaRestApiApplication {
 	git init
 	git add README.md
 	git commit -m "first commit"
-	git branch -M main
+	git branch -M master 
 	git remote add origin https://github.com/MahaCloud/MahaRestAPI.git
-	git push -u origin main
+	git push -u origin master OR go to Eclipse Push it 
 
 */
 	
